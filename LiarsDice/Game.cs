aaf0dee7c82759ff6ecb90ccf1d86f4afa6fb8df
Day <dlitt2@uis.edu>
2021-04-14 18:33:07 -0500

    TX-11-Documentation Added additional documentation/comments Removed unneccesary functions

--- LiarsDice/Game.cs
+++ LiarsDice/Game.cs
@@ -11,12 +11,16 @@ namespace LiarsDice
         Player[] players = new Player[4];
         int numOfPlayers;
         int betAmt; //the countable instances of the bet
-        int betNum; //the value shown on the dice/bet
+        int betNum; //the "face" value shown on the dice/bet
         int turn; //starts at 0 for turn one, goes up to 3
         int totalDice;
         static int SLEEPTIME = 2500;
         static int NAPTIME = 1500;
 
+        /// <summary>
+        /// Game constructor. Takes input num as the number of players.
+        /// </summary>
+        /// <param name="num">The number of players.</param>
         public Game(int num)
         {
             numOfPlayers = num;
@@ -30,79 +34,128 @@ namespace LiarsDice
             totalDice = 0;
         }
 
+        /// <summary>
+        /// Delays the game to provide for easier viewing for the user.
+        /// </summary>
         public void sleep()
         {
             Thread.Sleep(SLEEPTIME);
         }
 
+        /// <summary>
+        /// Delays the game to provide for easier viewing, but for less time than sleep.
+        /// </summary>
         public void nap()
         {
             Thread.Sleep(NAPTIME);
         }
 
+        /// <summary>
+        /// Sets turn to turnNum
+        /// </summary>
+        /// <param name="turnNum">Turn number provided</param>
         public void setTurn(int turnNum)
         {
             turn = turnNum;
         }
+
+        /// <summary>
+        /// Sets human to true inside the Player class.
+        /// </summary>
+        /// <param name="i">The index of the array</param>
         public void setHuman(int i)
         {
             players[i].setHuman();
         }
 
-        public void setNumOfPlayers(int num)
-        {
-            numOfPlayers = num;
-            totalDice = numOfPlayers * 5;
-        }
-
+        /// <summary>
+        /// Sets the current betAmt to the amt provided.
+        /// </summary>
+        /// <param name="amt">The new betAmt</param>
         public void setBetAmt(int amt)
         {
             betAmt = amt;
         }
 
+        /// <summary>
+        /// Sets the current betNum to the num provided.
+        /// </summary>
+        /// <param name="num">The new betNum</param>
         public void setBetNum(int num)
         {
             betNum = num;
         }
+
+        /// <summary>
+        /// Returns the numOfPlayers
+        /// </summary>
+        /// <returns>The number of players in the game.</returns>
         public int getNumOfPlayers()
         {
             return numOfPlayers;
         }
 
+        /// <summary>
+        /// Returns the betAmt
+        /// </summary>
+        /// <returns>The current betAmt equal to countable instances of a die</returns>
         public int getBetAmt()
         {
             return betAmt;
         }
 
+        /// <summary>
+        /// Returns the betNum
+        /// </summary>
+        /// <returns>The current betNum equal to the face of the die</returns>
         public int getBetNum()
         {
             return betNum;
         }
 
-        public int getNumOfDice(int i)
-        {
-            return players[i].getNumOfDice();
-        }
-
+        /// <summary>
+        /// Returns the Player of players[i]. Allows for use of the Player class functions.
+        /// </summary>
+        /// <param name="i">Index of the array</param>
+        /// <returns>A Player object</returns>
         public Player getPlayer(int i)
         {
             return players[i];
         }
 
+        /// <summary>
+        /// Returns the index of the current Player in players[i]
+        /// </summary>
+        /// <returns>The index of the current Player in the array players[i]</returns>
         public int getTurn()
         {
             return turn;
         }
 
+        /// <summary>
+        /// Returns the totalDice
+        /// </summary>
+        /// <returns>The total amount of dice in the game.</returns>
         public int getTotalDice()
         {
             return totalDice;
         }
 
+        /// <summary>
+        /// Returns false if the current player is not an ai/computer.
+        /// </summary>
+        /// <param name="i">The index of the players[i] array</param>
+        /// <returns>Returns true if the player at index i is the human player. False if current player is the ai/computer.</returns>
         public bool isHuman(int i)
         {
             return players[i].isHuman();
         }
+
+        /// <summary>
+        /// Removes a player who has run out of all their dice. 
+        /// Reformats the players[] array to avoid NullPointerException
+        /// </summary>
+        /// <param name="spot">The index of the Player to be removed in players[]</param>
         public void removePlayer(int spot)
         {
             if (spot == 0)
--- LiarsDice/Player.cs
+++ LiarsDice/Player.cs
@@ -11,16 +11,10 @@ namespace LiarsDice
         int numOfDice;
         int playerNumber;
 
-        public Player()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                dice[i] = 0;
-            }
-            human = false;
-            numOfDice = 5;
-        }
-
+        /// <summary>
+        /// Player constructor. Takes input num and assigns the player with the playerNumber of num.
+        /// </summary>
+        /// <param name="num">Variable to be assigned to playerNum.</param>
         public Player(int num)
         {
             for (int i = 0; i < 5; i++)
@@ -32,16 +26,26 @@ namespace LiarsDice
             playerNumber = num;
         }
 
+        /// <summary>
+        /// Returns the playerNumber
+        /// </summary>
+        /// <returns>Returns the playerNumber</returns>
         public int getPlayerNumber()
         {
             return playerNumber;
         }
 
+        /// <summary>
+        /// Sets the variable human to true.
+        /// </summary>
         public void setHuman()
         {
             human = true;
         }
 
+        /// <summary>
+        /// Randomizes each die to faces between 1 and 6 inclusively.
+        /// </summary>
         public void setDice()
         {
             Random rnd = new Random();
@@ -51,26 +55,37 @@ namespace LiarsDice
             }
         }
 
+        /// <summary>
+        /// Returns false if the current player is not an ai/computer.
+        /// </summary>
+        /// <returns>Returns true if current player is the human. Returns false if the current player is the ai/computer.</returns>
         public bool isHuman()
         {
             return human;
         }
 
+        /// <summary>
+        /// Returns the dice face of array spot i in dice[].
+        /// </summary>
+        /// <param name="i">The index of the array.</param>
+        /// <returns>The dice face of the index provided.</returns>
         public int getDice(int i)
         {
             return dice[i];
         }
 
-        public int[] getDiceArray()
-        {
-            return dice;
-        }
-
+        /// <summary>
+        /// Returns the variable numOfDice
+        /// </summary>
+        /// <returns>Returns the variable numOfDice</returns>
         public int getNumOfDice()
         {
             return numOfDice;
         }
 
+        /// <summary>
+        /// Removes a die whenever the player loses a bet.
+        /// </summary>
         public void removeDie()
         {
             numOfDice -= 1;
